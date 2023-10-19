6c55328960b75193e1be06fb3ee0721e6b7d34df
s18486_sci.pdn <s18486@sci.pdn.ac.lk>
2023-10-19 10:39:49 +0530

    Create password encoding methods

--- demo/src/main/java/com/ehealthpera/demo/Repository/UserRepo.java
+++ demo/src/main/java/com/ehealthpera/demo/Repository/UserRepo.java
@@ -6,6 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepo extends JpaRepository<User,String> {
 
+    User findByUserEmail(String email);
     /*@Query(value= "SELECT * FROM USER WHERE ID= ?1",nativeQuery = true)
     User getUserById(String Id);*/
 }
+
+
--- demo/src/main/java/com/ehealthpera/demo/Service/UserService.java
+++ demo/src/main/java/com/ehealthpera/demo/Service/UserService.java
@@ -17,7 +17,20 @@ public class UserService {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Autowired
+    private PasswordEncorder passwordEncorder;
 
+    public User registerUser(User user){
+        //Check if the user email is already taken
+        if(userRepo.findByUserEmail(user.getEmail()) != null){
+            throw new RuntimeException("User already exists");
+        }
+
+        //Encrypt the user's password before saving it
+        user.setPassword(passwordEncorder.encode(user.getPAssword()));
+
+        return userRepo.save(user);
+    }
     public UserDTO saveUser(UserDTO userDTO){
         //userRepo.save(new User(userDTO.getId()));
 //        userRepo.save(modelMapper.map(userDTO, User.class));
