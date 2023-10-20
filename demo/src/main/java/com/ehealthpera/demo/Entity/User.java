32afa4b7a2da78955b0868bcecfd81ec34a5164e
s18486_sci.pdn <s18486@sci.pdn.ac.lk>
2023-10-20 10:52:05 +0530

    Create get all user methods

--- demo/src/main/java/com/ehealthpera/demo/Entity/User.java
+++ demo/src/main/java/com/ehealthpera/demo/Entity/User.java
@@ -4,27 +4,34 @@ package com.ehealthpera.demo.Entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.*;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Table(name ="user")
 public class User {
     @Id
+    @Column(name = "id")
     private String id;
 
-    @Column(nullable = false)
+    @Column(name="name",nullable = false)
     private String name;
 
-    @Column(nullable = false)
+    @Column(name="email",nullable = false)
     private String email;
 
-    @Column(nullable = false)
+    @Column(name="password",nullable = false)
     private String password;
 
-    @Column(nullable = false)
+    @Column(name="national_id",nullable = false)
     private String nationalId;
 
-    private int phoneNumber;
+    @Column(name="phone_number",nullable = false)
+    private String phoneNumber;
+
 }
